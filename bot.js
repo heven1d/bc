d3789a5d004f7ea74511089555a60f915d620e10
heven1d <44444524+heven1d@users.noreply.github.com>
2018-10-24 20:34:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "!";  
+var prefix = "-";  
+const prefix = "-"
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` seqύence  `,"http://twitch.tv/S-F")
+//client.user.setGame(` seqύence  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -69,7 +71,32 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '232564113459511306') return;
 
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
 
 
 

d70ad42a04b7fc00609c70c2e347936e3f94d301
heven1d <44444524+heven1d@users.noreply.github.com>
2018-10-24 20:57:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var prefix = "-";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-//client.user.setGame(` seqύence  `,"http://twitch.tv/S-F")
+client.user.setGame(` seqύence  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')

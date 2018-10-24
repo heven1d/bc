9c1c64df86e6b8eefb9221e7eb0b39b3f81b94f5
heven1d <44444524+heven1d@users.noreply.github.com>
2018-10-24 20:54:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,32 +71,6 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-  if (!message.content.startsWith(-)) return;
-  var args = message.content.split(' ').slice(1);
-  var argresult = args.join(' ');
-  if (message.author.id !== '232564113459511306') return;
-
-if (message.content.startsWith('-g')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith('-w')) {
-client.user.setActivity(argresult, {type:'WATCHING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-if (message.content.startsWith('-l')) {
-client.user.setActivity(argresult, {type:'LISTENING'});
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-} else 
-
-if (message.content.startsWith('-s')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
-    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
-}
-
-});
 
 
 

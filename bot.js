6b308dfc73a4a53677f58b071de1e38b7114da0d
heven1d <44444524+heven1d@users.noreply.github.com>
2018-10-25 16:36:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 //client.user.setGame(` seqύence  `)
-client.user.setGame(` seqύence | BC  `,"http://twitch.tv/S-F")
+client.user.setGame(` Oblivion | BC  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')

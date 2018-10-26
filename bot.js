03e72e59c7da021a89e9dc3b1c31d8b3ac26eb63
heven1d <44444524+heven1d@users.noreply.github.com>
2018-10-26 15:54:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,72 +1,167 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var prefix = "!"
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-//client.user.setGame(` seqύence  `)
-client.user.setGame(` Oblivion | BC  `,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online ')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
+
+
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("!obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "!";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
+
+
+
+
 
 client.on('message', message => {
-    if (message.content.split(' ')[0] == '-bc')
-       message.guild.members.forEach( member => {
-         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    var  user = message.mentions.users.first() || message.author;
+if (message.content.startsWith("b!avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
 
 
-           
-              member.send( "**[" + ` ${member} ` + "]**" + "  " + message.content.substr(3));                                        message.delete();
-            
-                                                    });
-            
-                                                  });
-client.on("message", message => {
-       var prefix = "-";
- 
-             var args = message.content.substring(prefix.length).split(" ");
-                if (message.content.startsWith(prefix + "bc")) {
-                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
 
-                          if (!args[1]) {
-                            
-                                 let embed3 = new Discord.RichEmbed()
-                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
-                                       .setColor("#FF00FF")
-                                          message.channel.sendEmbed(embed3);
-                            
-                                        } else {
 
-                            
-                                           let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
-                                                                .setColor("#99999")
-                               
-                                                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
+
+
+
+  client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('b!adminbcdwdwdwdwdwdwdwd')){
+if(!message.author.id === '476185102922285066') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
 });
 
 
 
-client.login(process.env.BOT_TOKEN);
+  
+  client.on('message', msg => {
+    if(msg.content === 'b!helpdwdwdwdw')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  client.on("message", message => {
+    if (message.content === "b!helpwdwdwdw") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+
+       b!obc | لأرسال برود كاست للكل
+
+       b!bc  |  لأرسال برود كاست للأونلاين
+
+       b!invite | لدعوة البوت الي سيرفرك
+
+       b!support | سيرفر السبورت** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+
+
+   client.on("message", message => {
+    if (message.content === "b!invdwdwdwdwdwdwdwdwdite") {
+     const embed = new Discord.RichEmbed()
+         .setColor("RANDOM")
+         .addField('Broadcast', `https://discordapp.com/api/oauth2/authorize?client_id=489960386339274771&permissions=8&scope=bot`)
+     message.author.send({embed});
+   
+    }
+   });
+
+
+   client.on("message", message => {
+    if (message.content === "b!suppdwdwdwdwdwdwdwort") {
+     const embed = new Discord.RichEmbed()
+         .setColor("RANDOM")
+         .addField('❤سيرفر الدعم الفني', `  https://discord.gg/NuQ2zYu  `)
+     message.author.send({embed});
+   
+    }
+   });
+
+
+
+
+
+   client.on('message', message => {
+    if (message.content.startsWith("b!bwdwdwdwdwdwdwdot")) {
+    message.channel.send({
+        embed: new Discord.RichEmbed()
+            .setAuthor(client.user.username,client.user.avatarURL)
+            .setThumbnail(client.user.avatarURL)
+            .setColor('RANDOM')
+            .setTitle('``INFO Broadcast Bot`` ')
+            .addField('``My Ping``' , [`${Date.now() - message.createdTimestamp}` + 'MS'], true)
+            .addField('``RAM Usage``', `[${(process.memoryUsage().rss / 1048576).toFixed()}MB]`, true)
+            .addField('``servers``', [client.guilds.size], true)
+            .addField('``channels``' , `[ ${client.channels.size} ]` , true)
+            .addField('``Users``' ,`[ ${client.users.size} ]` , true)
+            .addField('``My Name``' , `[ ${client.user.tag} ]` , true)
+            .addField('``My ID``' , `[ ${client.user.id} ]` , true)
+                  .addField('``My Prefix``' , `[-]` , true)
+                  .addField('``My Language``' , `[ Java Script ]` , true)
+                    })
+}
+});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("!bc",{type: 'WATCHING'});
+  
+  });
+
+client.login(process.env.TOKEN);
